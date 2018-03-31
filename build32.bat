6a464a65002d24fc0479a368f627800afc1ba990
horsicq <horsicq@gmail.com>
2018-04-01 00:30:57 +0200

    Fix

--- build32.bat
+++ build32.bat
@@ -1,4 +1,4 @@
-set QMAKE="C:\Qt\qt-5.6.0-x86-msvc2013\5.6\msvc2013\bin\qmake.exe"
+set QMAKE="C:\Qt\qt-5.6.2-x86-msvc2013\5.6\msvc2013\bin\qmake.exe"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
 
--- build64.bat
+++ build64.bat
@@ -1,4 +1,4 @@
-set QMAKE="C:\Qt\qt-5.6.0-x64-msvc2013\5.6\msvc2013_64\bin\qmake.exe"
+set QMAKE="C:\Qt\qt-5.6.2-x64-msvc2013\5.6\msvc2013_64\bin\qmake.exe"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64\vcvars64.bat"
 
