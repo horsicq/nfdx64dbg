f960b827f8788670a0979a04d5c234eae122b16f
horsicq <horsicq@gmail.com>
2019-02-21 20:37:30 +0100

    Fix

--- nfd/PluginMainWindow.cpp
+++ nfd/PluginMainWindow.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018 hors<horsicq@gmail.com>
+// Copyright (c) 2017-2019 hors<horsicq@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -49,8 +49,7 @@ void PluginMainWindow::on_pushButtonScan_clicked()
         ds.setData(sFileName,&options,&scanResult);
         ds.exec();
 
-        ui->treeViewResult->setModel(new StaticScanItemModel(&scanResult.listRecords));
-        ui->treeViewResult->expandAll();
+        ui->widgetResult->setData(&scanResult,"");
     }
 }
 
